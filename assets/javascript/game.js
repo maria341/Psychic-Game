9a082958a022a1eb84f139f94be3fa16334a2eaa
Maria may Roberts <mariamayr96@gmail.com>
2019-09-06 16:09:42 -0700

    index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 //Creating an array of letters for computer choices
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var computerChoices = ["a", "b", "c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var letterOption = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
 
 //Set all variables to zero
 var wins = 0;
@@ -14,15 +16,14 @@ document.onkeyup = function(event) {
 
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.lenght)];
 
-//random letters for player option
-    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-		 	
-    if (options.indexOf(userGuess) > -1) {
+     if (letterOption.indexOf(userGuess) > -1) {
    
          if (userGuess === computerGuess) {
              wins++;
              guessesLeft = 9;
              playerChoices = [];
+             document.querySelector("wins").innerHTML = "WINS:" + wins;
+             restart();
          }
          else {
             if (playerChoices.includes(userGuess)) {
@@ -39,14 +40,18 @@ document.onkeyup = function(event) {
             losses++;
             guessesLeft = 9;
             playerChoices = [];
+            document.querySelector("losses").innerHTML = "LOSSES:" + losses;
+            restart();
             
         } 
             
-         var html = "<h1>Psychic Game</h1>" + "<h2>What letter I'm thinking of!</h2>" +
-                       "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" +
-                        "<p>Guesses Left:" + guessesLeft + "</p>" + "<p>Your Guesses so far:" + playerChoices.join(", ") + "</p>";
+         var html = "<h1>Psychic Game</h1>" + "<h2>What letter I'm thinking of!</h2>" + 
+         
+         "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" +
+        
+         "<p>Guesses Left:" + guessesLeft + "</p>" + "<p>Your Guesses so far:" + playerChoices.join(", ") + "</p>";
 
-                         document.querySelector("#game").innerHTML = html;
+         document.querySelector("#mainContainer").innerHTML = html;
         
             
         }
